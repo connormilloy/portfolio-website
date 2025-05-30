ab70ebfe902d7eb5046a3a98a92a4afb0b78f05a
connormilloy <connorking.milloy@gmail.com>
2025-05-31 00:27:52 +0100

    regrettable 3am code

--- src/components/screens/Desktop/Desktop.jsx
+++ src/components/screens/Desktop/Desktop.jsx
@@ -1,31 +1,63 @@
+import { useState, useEffect } from 'react';
 import STYLES from './Desktop.module.scss';
 import Taskbar from './Taskbar';
 
 import icon from './Taskbar/StartButton/images/icons/fallback.png';
-import DesktopIcon from './DesktopIcon/DesktopIcon.jsx';
+import projectData from '../../../data/projectData.json';
+
+import DesktopIcon from './DesktopIcon';
+import Window from './Window';
+
 const Desktop = () => {
+  const [openWindows, setOpenWindows] = useState({});
+
+  useEffect(() => {
+    const initialWindows = {};
+    Object.entries(projectData).forEach((project) => {
+      initialWindows[project.name] = false;
+    });
+    setOpenWindows(initialWindows);
+  }, []);
+
+  const openWindow = (project) => {
+    if (openWindows[project]) return;
+    setOpenWindows((prevState) => ({
+      ...prevState,
+      [project]: true,
+    }));
+  };
+
+  const closeWindow = (project) => {
+    setOpenWindows((prevState) => ({
+      ...prevState,
+      [project]: false,
+    }));
+  };
+
   return (
     <div className={STYLES.Desktop}>
       <div className={STYLES.Desktop__icons}>
-        <DesktopIcon
-          icon={icon}
-          label={'A project'}
-          altText={'A project icon'}
-          onClick={() => alert('A project clicked!')}
-        />
-        <DesktopIcon
-          icon={icon}
-          label={'A project'}
-          altText={'A project icon'}
-          onClick={() => alert('A project clicked!')}
-        />
-        <DesktopIcon
-          icon={icon}
-          label={'A project'}
-          altText={'A project icon'}
-          onClick={() => alert('A project clicked!')}
-        />
+        {Object.entries(projectData).map(([key]) => (
+          <DesktopIcon
+            key={key}
+            icon={icon}
+            label={key}
+            altText={`${key} icon`}
+            onClick={() => openWindow(key)}
+          />
+        ))}
       </div>
+
+      {Object.keys(openWindows).map(
+        (project) =>
+          openWindows[project] && (
+            <Window
+              key={project}
+              project={projectData[project]}
+              onClose={() => closeWindow(project)}
+            />
+          )
+      )}
       <Taskbar />
     </div>
   );
--- src/components/screens/Desktop/Window/Window.jsx
+++ src/components/screens/Desktop/Window/Window.jsx
@@ -1,3 +1,34 @@
-const Window = () => {};
+import { useState, useEffect } from 'react';
+
+import STYLES from './Window.module.scss';
+
+const Window = ({ project, onClose }) => {
+  const { name, description, imageUrl } = project;
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    if (!imageUrl) {
+      import('../../../../images/project1.png').then((image) =>
+        setImage(image.default)
+      );
+    }
+  }, []);
+
+  if (!image) return null;
+
+  return (
+    <div className={STYLES.Window}>
+      <div className={STYLES.Window__header}>
+        <span className={STYLES.Window__title}>{name}</span>
+        <button className={STYLES.Window__closeButton} onClick={onClose}>
+          X
+        </button>
+      </div>
+      <div className={STYLES.WindowContent}>
+        <img src={image} className={STYLES.WindowContent__image} />
+      </div>
+    </div>
+  );
+};
 
 export default Window;
